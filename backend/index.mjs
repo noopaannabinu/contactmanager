7e754d1eb7212ee347c0f3c42addac10474c3102
HEYRON <68512524+heyronjmilton@users.noreply.github.com>
2023-11-02 01:14:54 +0530

    init

--- backend/index.mjs
+++ backend/index.mjs
@@ -1,1 +1,21 @@
+import 'dotenv/config'
+import express from 'express'
+import bodyParser from 'body-parser';
 
+const app = express();
+
+const port = 8000
+
+app.use(express.json());
+app.use(bodyParser.json())
+
+app.get('/',(req,res)=>{
+    console.log(req.params)
+    res.json({data : "hello serveer is working"})
+})
+
+import userroute from './routes/route.mjs'
+
+app.listen(port,() => {
+    console.log(`server started at http://localhost:${port}`)
+})
