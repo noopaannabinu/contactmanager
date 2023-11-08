7f8a5c8ddb95da1d0879cb17c57e295ab4842098
HEYRON <68512524+heyronjmilton@users.noreply.github.com>
2023-11-08 21:47:13 +0530

    test

--- backend/database/database.mjs
+++ backend/database/database.mjs
@@ -7,7 +7,7 @@ const  {
     MONGO_DB_NAME,
   } = process.env;
 
-  const mongoose_uri = `mongodb://127.0.0.1:27017`;
+  const mongoose_uri = `mongodb://127.0.0.1:27017/contact_manger`;
 
   const db = await mongoose.connect(mongoose_uri, {
     useNewUrlParser: true,
@@ -15,5 +15,48 @@ const  {
     serverSelectionTimeoutMS: 5000,
   });
   
-  console.log(db)
-  db.close()
+const data ={name:{
+    fname :"test",
+    mname :"mname",
+    lname: "lname"
+}, 
+age:30,
+phone : "9495952562",
+email : "test@test.com" 
+}
+
+
+const contact_schema = mongoose.Schema({
+
+  name: {
+    type: JSON,
+    required: true,
+    default : null
+  },
+
+  // loc : location
+  age : {
+    type : Number
+  },
+  // ph: phone number
+  phone: {
+    type: String,
+    //default: null,
+    required : true
+  },
+  //gm : merchant's email
+  email : {
+    type : String,
+    //required : true,
+    default : null
+  }
+},{timestamps : true});
+
+export const Contact = new mongoose.model(
+  'contacts',
+  contact_schema
+);
+const contact = new Contact(data)
+const res  = await contact.save()
+console.log(res)
+mongoose.connection.close()
---
+++ backend/test.js
@@ -0,0 +1,9 @@
+const data ={"name":{
+    fname :"faname",
+    mname :"mname",
+    lname: "lname"
+}, 
+"age":30, 
+"car":null}
+
+console.log(data.name.fname)
