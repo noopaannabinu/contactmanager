ca1f739b8a1506e2f0f48e9ebb165a5e4e515eba
HEYRON <68512524+heyronjmilton@users.noreply.github.com>
2023-11-08 21:51:33 +0530

    update

--- backend/database/database.mjs
+++ backend/database/database.mjs
@@ -1,6 +1,6 @@
 import 'dotenv/config'
 import mongoose from 'mongoose'
-
+import { Contact } from '../models/contacts.js';
 const  {
     MONGODB_SERVERLESS_USERNAME,
     MONGODB_SERVERLESS_PASSWORD,
@@ -26,36 +26,7 @@ email : "test@test.com"
 }
 
 
-const contact_schema = mongoose.Schema({
 
-  name: {
-    type: JSON,
-    required: true,
-    default : null
-  },
-
-  // loc : location
-  age : {
-    type : Number
-  },
-  // ph: phone number
-  phone: {
-    type: String,
-    //default: null,
-    required : true
-  },
-  //gm : merchant's email
-  email : {
-    type : String,
-    //required : true,
-    default : null
-  }
-},{timestamps : true});
-
-export const Contact = new mongoose.model(
-  'contacts',
-  contact_schema
-);
 const contact = new Contact(data)
 const res  = await contact.save()
 console.log(res)
---
+++ backend/models/contacts.js
@@ -0,0 +1,35 @@
+import 'dotenv/config'
+import mongoose from 'mongoose'
+
+
+
+const contact_schema = mongoose.Schema({
+
+    name: {
+      type: JSON,
+      required: true,
+      default : null
+    },
+  
+    // loc : location
+    age : {
+      type : Number
+    },
+    // ph: phone number
+    phone: {
+      type: String,
+      //default: null,
+      required : true
+    },
+    //gm : merchant's email
+    email : {
+      type : String,
+      //required : true,
+      default : null
+    }
+  },{timestamps : true});
+  
+  export const Contact = new mongoose.model(
+    'contacts',
+    contact_schema
+  );
